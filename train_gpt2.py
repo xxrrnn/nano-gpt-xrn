9b2a4eb6614a1883c78a4256b9eef9e97a0913a9
xxrrnn <xrn2019@126.com>
2025-05-11 19:59:05 +0800

    [2401111907] add a DataLoaderLite

--- train_gpt2.py
+++ train_gpt2.py
@@ -117,7 +117,7 @@ class GPT(nn.Module):
         # forward the final layernorm and the classifier
         x = self.transformer.ln_f(x)
         logits = self.lm_head(x) # (B, T, vocab_size)
-        
+
         loss = None
         if targets is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)),targets.view(-1))
@@ -171,6 +171,38 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
         return model
     
+import tiktoken
+
+class DataLoaderLite:
+    def __init__(self, B, T):
+        self.B = B
+        self.T = T
+
+        # at init load tokens from disk and store them in memory
+        with open('input.txt', 'r') as f:
+            text = f.read()
+        enc = tiktoken.get_encoding('gpt2')
+        tokens = enc.encode(text)
+        self.tokens = torch.tensor(tokens)
+        print(f"loaded {len(self.tokens)} tokens")
+        print(f"1 epoch = {len(self.tokens) // (B * T)} batches")
+
+        # state
+        self.current_position = 0
+    
+    def next_batch(self):
+        B, T = self.B, self.T
+        buf = self.tokens[self.current_position: self.current_position + B*T + 1]
+        x = (buf[:-1]).view(B, T)
+        y = (buf[1:]).view(B,T)
+        # advance the position in the tensor
+        self.current_position += B * T
+        # if loading the next batch would be out of bounds, reset
+        if self.current_position + (B * T + 1) > len(self.tokens):
+            self.current_position = 0
+        return x, y
+    
+
 # --------------------------------------
 device = "cpu"
 if torch.cuda.is_available():
@@ -179,18 +211,7 @@ elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
     device = "mps"
 print(f"using device: {device}")
 
-# get a data batch
-import tiktoken
-enc = tiktoken.get_encoding('gpt2')
-with open('input.txt', 'r') as f:
-    text = f.read()
-text = text[:1000]
-tokens = enc.encode(text)
-B, T = 4,32
-buf = torch.tensor(tokens[:B*T + 1])
-buf = buf.to(device)
-x = buf[:-1].view(B,T)
-y = buf[1:].view(B,T)
+train_loader = DataLoaderLite(B = 4, T = 32)
 
 # get logits
 model = GPT(GPTConfig())
@@ -199,6 +220,8 @@ model.to(device)
 
 optimizer = torch.optim.AdamW(model.parameters(), lr = 3e-4)
 for i in range(50):
+    x, y = train_loader.next_batch()
+    x,y = x.to(device), y.to(device)
     optimizer.zero_grad()
     logits, loss = model(x, y)
     loss.backward()
